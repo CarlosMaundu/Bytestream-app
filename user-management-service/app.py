3d3b994f0ec3fd39464e8f41bb8d13f5aad1d92f
carlos <carlmaundu@gmail.com>
2024-08-12 22:35:52 +0300

    Bytestream code

---
+++ user-management-service/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/usermanagement')
+def user_management():
+    return "Welcome to the User Management Service!"
+
+@app.route('/healthz')
+def healthz():
+    return "OK", 200
+
+@app.route('/ready')
+def ready():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
